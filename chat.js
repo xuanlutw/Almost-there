d7377de064a02cd1cd13ed831fdc1dc006a8371a
m24639297 <m24639297@gmail.com>
2018-05-26 07:10:08 +0800

    Welcome message and broadcast content

--- chat.js
+++ chat.js
@@ -1,7 +1,7 @@
 const a = require('./chat.json')
 
-let key = []
-let res = []
+const key = []
+const res = []
 // console.log(a)
 for (var i = 0; i < a.length; i++) {
 	// console.log(a[i])
@@ -24,4 +24,20 @@ function find(keyword){
 function chat(input){
 	if(find(input) === false) return ["Say something else!"]
 	return(res[find(input)])
+}
+//=============== Hello message ====================
+function welcome(){
+	return "感謝您使用Weather x Health小幫手！我是您健康的守護者！您會在天氣開始變化時，收到精選的注意事項。另外，也可以跟我聊聊天喔！"
+}
+
+//=================== Broadcast ================
+
+b = require('./activerespond-2.json')
+const content = []
+for(i in b['active'])for(j in b['active'][i]){
+	content.push(b['active'][i][j]['string'])
+}
+
+function news() {
+	return content[Math.floor((Math.random() * content.length) + 1)]
 }
